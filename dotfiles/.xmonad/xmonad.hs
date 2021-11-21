8bd04743eb86e0bc9520fc0cdf61dc3ca7071dae
hmanhng <hmanhng@icloud.com>
2021-11-21 12:17:53 +0700

    unimatrix

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -134,11 +134,11 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm]
 
 myStartupHook :: X ()
 myStartupHook = do
+    spawnOnce "alacritty -o window.startup_mode=Fullscreen background_opacity=1 -e unimatrix -l knS -c cyan -s 90"
     spawnOnce "lxsession &"
     spawnOnce "picom &"
     spawnOnce "xcape -e 'Super_L=Alt_L|F1'"
-    spawnOnce "alacritty -e unimatrix -l knS -c cyan -s 90"
-    spawnOnce "alacritty -e $HOME/.config/gotop/gotop"
+    --spawnOnce "alacritty -e $HOME/.config/gotop/gotop"
     spawnOnce "conky -c $HOME/.config/conky/xmonad/doom-one-01.conkyrc"
     spawnOnce "trayer --edge top --align right --widthtype request --padding 0 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 24 &"
     spawnOnce "nowater -d ~/Pictures/Wallpapers/ -t 1m"
